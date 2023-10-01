a6b328329810da6bc0f01ba55f12426c219b99ed
reddevilmidzy <midzy0228@gmail.com>
2023-10-01 20:36:54 +0900

    test: 자리는 다르고 숫자만 같은 경우 - 볼

--- src/test/java/baseball/controller/ScoreTest.java
+++ src/test/java/baseball/controller/ScoreTest.java
@@ -19,6 +19,15 @@ class ScoreTest {
     }
 
     @Test
+    @DisplayName("자리는 다르고 숫자만 같은 경우 - 볼")
+    void calculateBallTest() {
+        assertThat(new Score(List.of(1,2,3), List.of(1,2,3)).calculateBall()).isEqualTo(0);
+        assertThat(new Score(List.of(5,6,7), List.of(1,5,7)).calculateBall()).isEqualTo(1);
+        assertThat(new Score(List.of(4,5,6), List.of(5,4,9)).calculateBall()).isEqualTo(2);
+        assertThat(new Score(List.of(7,8,9), List.of(8,9,7)).calculateBall()).isEqualTo(3);
+    }
+
+    @Test
     void calculateBall() {
     }
 }
