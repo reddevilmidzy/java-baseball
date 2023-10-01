d30886cbf9c1f141b8930828ed5b69a460c723ba
reddevilmidzy <midzy0228@gmail.com>
2023-10-02 01:08:18 +0900

    test: 사용자 입력 확인 테스트

---
+++ src/test/java/baseball/validation/InputValidatorTest.java
@@ -0,0 +1,29 @@
+package baseball.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InputValidatorTest {
+    Validator inputValidator = new InputValidator();
+
+    @Test
+    @DisplayName("올바른 사용자 입력인지 확인")
+    void checkValidValue() {
+        assertThat(inputValidator.isValidate("123")).isTrue();
+        assertThat(inputValidator.isValidate("948")).isTrue();
+        assertThat(inputValidator.isValidate("765")).isTrue();
+    }
+
+    @Test
+    @DisplayName("잘못된 사용자 입력 확인")
+    void checkInvalidValue() {
+        assertThat(inputValidator.isValidate("012")).isFalse();
+        assertThat(inputValidator.isValidate("989")).isFalse();
+        assertThat(inputValidator.isValidate("87")).isFalse();
+        assertThat(inputValidator.isValidate("1235")).isFalse();
+        assertThat(inputValidator.isValidate("red")).isFalse();
+        assertThat(inputValidator.isValidate("")).isFalse();
+    }
+}
