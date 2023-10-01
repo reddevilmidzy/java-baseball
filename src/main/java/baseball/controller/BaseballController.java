0db621daedec3c8ab0404f795d341409f0edc347
reddevilmidzy <midzy0228@gmail.com>
2023-10-01 21:02:45 +0900

    feat: 게임 종료 문구 출력

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -3,6 +3,7 @@ package baseball.controller;
 import baseball.converter.TypeConvert;
 import baseball.utils.GenerateRandomNumbers;
 import baseball.validation.InputValidator;
+import baseball.validation.RestartValidator;
 import baseball.validation.Validator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
@@ -20,18 +21,20 @@ public class BaseballController {
 
     public void playGame() {
         outputView.printStartMessage();
-        Validator validator = new InputValidator();
+
+        Validator inputValidator = new InputValidator();
+        Validator restartValidator = new RestartValidator();
 
         List<Integer> computers = GenerateRandomNumbers.generate();
-        System.out.println("computers = " + computers);
 
         boolean finished = false;
 
         while (!finished) {
+            System.out.println("computers = " + computers);
             String number = inputView.inputNumber();
 
-            if (!validator.isValidate(number)) {
-                throw new IllegalArgumentException();
+            if (!isValidate(inputValidator, number)) {
+                throw new IllegalArgumentException("숫자 입력 오류");
             }
 
             List<Integer> users = TypeConvert.mapIntegerList(number);
@@ -43,10 +46,22 @@ public class BaseballController {
             outputView.printGameScore(strike, ball);
 
             if (strike == 3) {
-                finished = true;
                 outputView.printEndMessage();
+                String restart = inputView.inputRestartOrNot();
+                if (!isValidate(restartValidator, restart)) {
+                    throw new IllegalArgumentException("재시작 입력 오류");
+                }
+                if (restart.equals("1")) {
+                    computers = GenerateRandomNumbers.generate();
+                } else if (restart.equals("2")) {
+                    finished = true;
+                }
             }
         }
+    }
 
+
+    private Boolean isValidate(Validator validator, String value) {
+        return validator.isValidate(value);
     }
 }
